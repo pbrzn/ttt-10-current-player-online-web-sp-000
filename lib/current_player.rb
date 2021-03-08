0223aa96e95c4560fa457833eb0ab1f1dcce5168
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-08 20:55:05 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+turns=0
+player="X"
+def turn_count(board)
+  board.each do |space|
+    if space=="X" || space=="O"
+      turns+=1
+    end
+  end
+end
+
+def current_player(board)
+  if turns%2==0
+    player="X"
+  else
+    player="O"
+  end
+end
